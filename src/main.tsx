78e74363121eaf7e67b371db9bf89b20ef6c0ae7
largekite <largekite@gmail.com>
2025-11-06 09:16:18 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,10 @@
-// main.tsx full code
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // create src/index.css or remove this line
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
